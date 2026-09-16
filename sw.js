707b5bfad7bacad7890c5b328f60fdd8ff232fa4
seoj07617-stack <seoj07617@gmail.com>
2026-09-16 21:53:24 +0800

    deploy: sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+/* 知行金融学院 · Service Worker：网络优先、缓存兜底（离线可用） */
+"use strict";
+const VER = "aurum-v3";
+const CORE = [
+  "./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png",
+  "./css/base.css", "./css/components.css", "./css/views.css", "./css/animations.css",
+  "./js/core.js", "./js/srs.js", "./js/ui.js",
+  "./js/data/foundations.js", "./js/data/macro.js", "./js/data/markets.js", "./js/data/company.js",
+  "./js/data/technical.js", "./js/data/funds.js", "./js/data/models.js", "./js/data/discipline.js",
+  "./js/data/portfolio.js", "./js/data/capstone.js",
+  "./js/views/home.js", "./js/views/map.js", "./js/views/stage.js", "./js/views/lesson.js",
+  "./js/views/quiz.js", "./js/views/review.js", "./js/views/wrong.js", "./js/views/stats.js",
+  "./js/views/dojo.js", "./js/views/glossary.js"
+];
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(VER).then(c => c.addAll(CORE)).catch(()=>{}));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== VER).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  if(e.request.method !== "GET" || !e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if(res && res.ok){
+        const cp = res.clone();
+        caches.open(VER).then(c => c.put(e.request, cp)).catch(()=>{});
+      }
+      return res;
+    }).catch(() =>
+      caches.match(e.request, { ignoreSearch: true }).then(r => r || caches.match("./index.html"))
+    )
+  );
+});
