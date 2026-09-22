abeb7e27b7a66a4163e28e3014c072d19be768d0
seoj07617-stack <seoj07617@gmail.com>
2026-09-22 21:22:14 +0800

    chore: bump sw aurum-v13

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 知行金融学院 · Service Worker：离线缓存 + network-first 版本管理 */
 "use strict";
-const VER = "aurum-v12";
+const VER = "aurum-v13";
 const CORE = [
   "./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png",
   "./css/base.css", "./css/components.css", "./css/views.css", "./css/ai.css", "./css/animations.css",
